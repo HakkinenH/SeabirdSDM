bddf710be609a5fca6fe89662ca454fe4967002c
HakkinenH <henry.hakkinen@ioz.ac.uk>
2021-10-12 19:53:30 +0100

    minor updates to checkdependencies

--- Code/CheckDependencies.R
+++ Code/CheckDependencies.R
@@ -14,7 +14,24 @@ require(dismo)
 
 
 #check that required files and folders are present
-
-if(!dir.exists(paste0("./IntermediateOutput/SeabirdSDM/OutputTables/",sp_name,"/"))){
-  dir.create(paste0("./IntermediateOutput/SeabirdSDM/OutputTables/",sp_name))
+if(!dir.exists("./IntermediateOutput/CombineEnvData/EnvPlots/")){
+  dir.create("./IntermediateOutput/CombineEnvData/EnvPlots/")
 }
+if(!dir.exists("./IntermediateOutput/CombineEnvData/MarineLookup/")){
+  dir.create("./IntermediateOutput/CombineEnvData/MarineLookup/")
+}
+
+
+if(!dir.exists("./IntermediateOutput/SeabirdSDM/OccMap/")){
+  dir.create("./IntermediateOutput/SeabirdSDM/OccMap/")
+}
+if(!dir.exists("./IntermediateOutput/SeabirdSDM/OutputPlots/")){
+  dir.create("./IntermediateOutput/SeabirdSDM/OutputPlots/")
+}
+if(!dir.exists("./IntermediateOutput/SeabirdSDM/OutputTables/")){
+  dir.create("./IntermediateOutput/SeabirdSDM/OutputTables/")
+}
+if(!dir.exists("./IntermediateOutput/SeabirdSDM/rstates/")){
+  dir.create("./IntermediateOutput/SeabirdSDM/rstates/")
+}
+
